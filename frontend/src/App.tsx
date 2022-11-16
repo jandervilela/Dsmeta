97af4fa91bf1c9f948890613545fdbca204fcee9
JanderVilela <janderbkp@gmail.com>
2022-11-16 09:55:05 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
-function App() {
- return(
-  <h1>Olá mundo! </h1>
+import NotificationButton from "./Componentes/NotificationButton"
 
- )
+function App() {
+  return (
+    <>
+      <h1>Olá mundo! </h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
